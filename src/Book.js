b8394db0a63afffafa7aaf9846cca2d0e486e885
Andrew Alekseev <andrewhkg@hotmail.com>
2018-02-19 17:42:51 +0800

    Added a conditional rendering to separate author names with a comma

--- src/Book.js
+++ src/Book.js
@@ -26,7 +26,7 @@ class Book extends Component {
           </div>
           <div className="book-shelf-changer">
             <select value={book.shelf} onChange={(e) => {this.shelfChanger(book, e)}}>
-              <option value="none" disabled>Move to...</option>
+              <option value="" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
               <option value="read">Read</option>
@@ -36,7 +36,7 @@ class Book extends Component {
         </div>
         <div className="book-title">{book.title}</div>
         {book.authors? book.authors.map((author) =>
-          <div key={author} className="book-authors">{author}</div>
+          <div key={author} className="book-authors">{Array.isArray(book.authors)?book.authors.join(', '):''}</div>
         ):''}
       </div>
     )
