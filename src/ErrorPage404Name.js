4feb8ad680b1776688ae7b44a84708a8383462d5
Andrew Alekseev <andrewhkg@hotmail.com>
2018-02-22 23:29:08 +0800

    Added page 404

--- src/ErrorPage404Name.js
+++ src/ErrorPage404Name.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import Page404 from '../public/404NotFound.png'
 
 class ErrorPage404Name extends Component {
   render () {
     return (
-        <div style={{display: 'grid', margin: 20, justifyContent: 'center', alignItems: 'center'}}>
-          <img alt="Page Not Found" src={Page404} />
-          <h3>Page Not found...</h3>
-        </div>
+      <div>
+
+          <img className='notfound' alt="Page Not Found" src={Page404} />
+      
+        <h3 className='notfound' style={{display: 'grid', margin: 20, justifyContent: 'center', alignItems: 'center'}}>Sorry, but the page you are looking for has not been found.</h3>
+        <button className='button'>
+          <Link to="/">Go to main page</Link>
+        </button>
+       </div>
     )
   }
 }
